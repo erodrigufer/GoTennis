eab6fc5289a71068f3943dcfa0a80f2c35a419d5
erodrigufer <erodrigufer@gmail.com>
2022-02-21 17:06:53 +0100

    mysql db handling packages

---
+++ pkg/models/mysql/sessions.go
@@ -0,0 +1,27 @@
+// Package to explicitly handle the sessions mysql db
+package mysql
+
+import(
+"database/sql"
+"github.com/erodrigufer/GoTennis/pkg/models"
+)
+
+// Define a SessionModel type which wraps a sql.DB connection pool
+type SessionModel struct {
+	DB *sql.DB
+}
+
+// Insert new session into the db
+func (m *SessionModel) Insert(title,content,expires string) (int,error){
+	return 0, nil
+	}
+
+// Get Session from db, using its id
+func (m *SessionModel) Get(id int) (*models.Session, error) {
+	return nil, nil
+}
+
+// Return the 10 most recently created sessions
+func (m *Sessionmodel) Latest() ([]*models.Session, error){
+	return nil, nil
+	}
