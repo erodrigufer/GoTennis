55743d887c12d498f3dd184c7d9f21b31bd2a7a4
erodrigufer <erodrigufer@gmail.com>
2022-02-25 17:11:06 +0100

    validate data from POST request

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/erodrigufer/GoTennis/pkg/models"
 )
@@ -66,12 +68,13 @@ func (app *application) showSession(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, "show.page.tmpl", dynamicData)
 }
 
-// Use GET request to get a form to do a POST request for a tennis session
+// After GET request, respond with a form to do a POST request for a
+// new tennis session
 func (app *application) createSessionForm(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, "create.page.tmpl", nil)
 }
 
-// Use POST request to create a new tennis session
+// Create a new tennis session after receiving a POST request
 func (app *application) createSession(w http.ResponseWriter, r *http.Request) {
 
 	// First call r.ParseForm() which adds any data in POST request bodies
@@ -88,6 +91,43 @@ func (app *application) createSession(w http.ResponseWriter, r *http.Request) {
 	title := r.PostForm.Get("title")
 	content := r.PostForm.Get("content")
 	expires := r.PostForm.Get("expires")
+
+	// Map holds any validation errors for the data being sent through POST req.
+	errors := make(map[string]string)
+
+	// Check that title field is not blank and is not more than 100 char. long
+	// If this check fails, add a message to the errors, using the name of the
+	// field as the key.
+	if strings.TrimSpace(title) == "" {
+		errors["title"] = "This field cannot be blank"
+		// the RuneCountInString function counts the amount of characters in a
+		// UTF8 string, some characters have a size bigger than one byte, so
+		// using the built-in len() function would be inaccurate
+	} else if utf8.RuneCountInString(title) > 100 {
+		errors["title"] = "This field is too long (max. is 100 characters)"
+	}
+
+	// Check that the content field is not empty
+	if strings.TrimSpace(content) == "" {
+		errors["content"] = "This field cannot be empty"
+	}
+
+	// Check that the expires field is not empty and matches one of the
+	// permitted values ("1", "7" or "365")
+	if strings.TrimSpace(expires) == "" {
+		errors["expires"] = "This field cannot be blank"
+	} else if expires != "365" && expires != "7" && expires != "1" {
+		errors["expires"] = "This field is invalid"
+	}
+
+	// If there are any errors with the data validation, dump them in a plain
+	// text HTTP response and return fron the handler before inserting the data
+	// into the database
+	if len(errors) > 0 {
+		fmt.Fprintf(w, errors)
+		return
+	}
+
 	// Create a new session record in the database using the form data.
 	id, err := app.session.Insert(title, content, expires)
 	if err != nil {
