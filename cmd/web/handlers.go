f94787b1c72f3b13465bcb0bbe03efe074af43f0
erodrigufer <erodrigufer@gmail.com>
2022-03-07 14:55:20 +0100

    insert user signup info into mysql table

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -141,8 +141,24 @@ func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
 		app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
 		return
 	}
-	// Otherwise send a placeholder response (work in progress, this will change later!)
-	fmt.Fprintln(w, "Create a new user...")
+	// Try to create a new user record in the database. If the email already
+	// exists, add an error message to the form and re-display it
+	err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
+	// email address is already stored in the users table
+	if err == models.ErrDuplicateEmail {
+		form.Errors.Add("email", "Address is already in use")
+		app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
+		return
+	} else if err != nil {
+		// another kind of error happened, on the server-side
+		app.serverError(w, err)
+		return
+	}
+	// Otherwise add a confirmation flash message to the session confirming that
+	// their signup worked and asking them to log in
+	app.session.Put(r, "flash", "Your signup was successful. Please log in.")
+	// And redirect the user to the login page.
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Display the user login form...")
--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -2,8 +2,12 @@ package mysql
 
 import (
 	"database/sql"
+	"strings"
 
 	"github.com/erodrigufer/GoTennis/pkg/models"
+
+	"github.com/go-sql-driver/mysql" // mysql driver
+	"golang.org/x/crypto/bcrypt"     // Password hashing
 )
 
 type UserModel struct {
@@ -12,7 +16,34 @@ type UserModel struct {
 
 // Insert a new record to the users table
 func (m *UserModel) Insert(name, email, password string) error {
-	return nil
+
+	// Create a bcrypt hash of the plain-text password.
+	// 12 is the 'cost' of the hash, which correlates to the amount of
+	// iterations needed to calculate the hash
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+					    VALUES(?, ?, ?, UTC_TIMESTAMP())`
+	// Use the Exec() method to insert the user details and hashed password
+	// into the users table. If this returns an error, we try to type assert
+	// it to a *mysql.MySQLError object so we can check if the error number is
+	// 1062 and, if it is, we also check whether or not the error relates to
+	// our users_uc_email key by checking the contents of the message string.
+	// If it does, we return an ErrDuplicateEmail error. Otherwise, we just
+	// return the original error (or nil if everything worked).
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		// type assert error to  MYSQLError
+		if mysqlErr, ok := err.(*mysql.MySQLError); ok {
+			// 1062 is the error code for duplicate entry
+			if mysqlErr.Number == 1062 && strings.Contains(mysqlErr.Message, "users_uc_email") {
+				return models.ErrDuplicateEmail
+			}
+		}
+	}
+	return err
 }
 
 // Authenticate login attempt with email and password
