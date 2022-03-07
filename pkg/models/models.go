83e67177426800ded014bce619eefb411953c870
Hap <erodrigufer@gmail.com>
2022-03-07 12:10:45 +0000

    files to handle users table

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -5,7 +5,15 @@ import (
 	"time"
 )
 
-var ErrNoRecord = errors.New("models: no matching record found")
+var (
+	ErrNoRecord = errors.New("models: no matching record found")
+	// Error for when a user tries to login in with an incorrect email address or
+	// password
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	// Error for when a user tries to sign up with an email adress that is already
+	// found in the database (not unique)
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+)
 
 type Session struct {
 	ID      int
@@ -13,4 +21,12 @@ type Session struct {
 	Content string
 	Created time.Time
 	Expires time.Time
+}
+
+type User struct {
+	ID             int
+	Name           string
+	Email          string
+	HashedPassword []byte
+	Created        time.Time
 }
