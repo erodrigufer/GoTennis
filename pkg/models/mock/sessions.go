ea1536b2163fc3503a2591acc192bfb06d4170a5
erodrigufer <erodrigufer@gmail.com>
2022-03-21 20:04:40 +0100

    add mock sessions db

---
+++ pkg/models/mock/sessions.go
@@ -0,0 +1,32 @@
+package mock
+
+import (
+	"time"
+
+	"github.com/erodrigufer/GoTennis/pkg/models"
+)
+
+var mockSession = &models.Session{
+	ID:      1,
+	Title:   "An old silent pond",
+	Content: "An old silent pond...",
+	Created: time.Now(),
+	Expires: time.Now(),
+}
+
+type SessionModel struct{}
+
+func (m *SessionModel) Insert(title, content, expires string) (int, error) {
+	return 2, nil
+}
+func (m *SessionModel) Get(id int) (*models.Session, error) {
+	switch id {
+	case 1:
+		return mockSession, nil
+	default:
+		return nil, models.ErrNoRecord
+	}
+}
+func (m *SessionModel) Latest() ([]*models.Session, error) {
+	return []*models.Session{mockSession}, nil
+}
