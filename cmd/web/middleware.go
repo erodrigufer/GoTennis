75d7f2b6d240eca512252519f921695605495e00
erodrigufer <erodrigufer@gmail.com>
2023-04-20 00:17:20 +0200

    Bump go's version, improve Errorf in middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -58,7 +58,7 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 				w.Header().Set("Connection", "close")
 				// Call the app.serverError helper method to return a 500
 				// Internal Server response.
-				app.serverError(w, fmt.Errorf("%s", err))
+				app.serverError(w, fmt.Errorf("Recovering from panic %w", err))
 			}
 		}()
 		next.ServeHTTP(w, r)
